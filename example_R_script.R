0b55cb00bcdf13184a3cac72e536906f9fdb551e
nikosbosse <nikosbosse@gmail.com>
2020-04-10 16:02:00 +0200

    clean up. bug not resolved

--- example_R_script.R
+++ example_R_script.R
@@ -1,15 +1,18 @@
+library(rstan)
+
 K = 4 # number of models
 R = 1 # number of regions
 T = 100 # number of timesteps
 S = 80 # number of predictive samples
 
-
+# create an example array
 predict_sample_mat <- array(rnorm(K * R * T * S), c(T, R, S,K))
 
-y_mat <- array(rnorm(S), c(R, T))
+# create observed true values
+y_mat <- array(rnorm(S * R), c(R, T))
+
 
 model <- rstan::stan_model("stan/crps_test.stan")
-
 standata <- list(K = 4,
                  R = 1,
                  T = 100,
@@ -17,4 +20,12 @@ standata <- list(K = 4,
                  predict_sample_mat = predict_sample_mat, 
                  y = y_mat)
 
-optimizing(m, standata)
+rstan::sampling(model, data = standata)
+
+
+
+rstan::optimizing(model_code = model, data = standata)
+
+
+
+?optimizing
